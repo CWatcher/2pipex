494ae06f9e4efa5a834f9a272acc4d638ac68c7f
Carmine Watcher <cwatcher@student.21-school.ru>
2021-06-12 20:43:12 +0300

    [rf] +run_cmd()

--- main.c
+++ main.c
@@ -4,6 +4,19 @@
 #include <errno.h>	//UNAUTH
 #include "libft.h"
 
+void	run_cmd(const char *cmd, char *const *envp)
+{
+	char *argv[] = {".", NULL};
+	char *path = ft_strjoin("/bin/", cmd);
+	int r =	execve(path, argv, envp);
+	path = ft_free(path);
+	if (r < 0)
+	{
+		perror("Failed to execve()");
+		exit(errno);
+	}
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	if (argc != 5)
@@ -11,13 +24,5 @@ int	main(int argc, char *argv[], char *envp[])
 		puts("Error:\nThe number of arguments is not equal to 4"); //UNAUTHORIZED
 		exit(4);
 	}
-	char *argv1[] = {".", NULL};
-	char *path1 = ft_strjoin("/bin/", argv[2]);
-	int r =	execve(path1, argv1, envp);
-	path1 = ft_free(path1);
-	if (r < 0)
-	{
-		perror("Failed to execve()");
-		exit(errno);
-	}
+	run_cmd(argv[2], envp);
 }
