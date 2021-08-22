25bae7c74bcc7075a7bdab36883507f3095357e8
Carmine Watcher <cwatcher@student.21-school.ru>
2021-08-22 20:29:38 +0300

    t*.sh += sample usage, mkdir tst

--- t.sh
+++ t.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# sample usage: ./t.sh main.c cat "wc -l"
+mkdir tst
 < $1 $2 | $3 > tst/out.0.txt
 pipex "$1" "$2" "$3" tst/out.txt
 diff tst/out.0.txt tst/out.txt
--- tb.sh
+++ tb.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-< $1 $2 | $3 > tst/out.0.txt
-pipex_bonus "$1" "$2" "$3" tst/out.txt
+# sample usage: ./tb.sh main.c cat "grep int" "wc -l"
+mkdir tst
+< $1 $2 | $3 | $4 > tst/out.0.txt
+pipex "$1" "$2" "$3" "$4" tst/out.txt
 diff tst/out.0.txt tst/out.txt
