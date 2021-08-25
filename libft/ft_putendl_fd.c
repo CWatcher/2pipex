2f1174066d26ac9f13d8a8494e51560eb41e624f
Carmine Watcher <cwatcher@student.21-school.r>
2021-08-25 15:20:10 +0300

    [rf] exit_me(): putstr() -> putendl()

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -1,37 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exit_me.c                                          :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/21 17:05:01 by CWatcher          #+#    #+#             */
-/*   Updated: 2021/08/25 12:43:10 by CWatcher         ###   ########.fr       */
+/*   Created: 2020/11/14 11:45:31 by CWatcher          #+#    #+#             */
+/*   Updated: 2020/11/15 20:49:13 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <errno.h>
 #include "libft.h"
 
-void	exit_me(char *dynamic_error)
+void	ft_putendl_fd(char *s, int fd)
 {
-	int	status;
-
-	status = EXIT_SUCCESS;
-	ft_putstr_fd("Error\n", STDERR_FILENO);
-	if (errno)
-	{
-		perror(dynamic_error);
-		status = errno;
-	}
-	else
-	{
-		ft_putendl_fd(dynamic_error, STDERR_FILENO);
-		status = EXIT_FAILURE;
-	}
-	free(dynamic_error);
-	exit(status);
+	write(fd, s, ft_strlen(s) * sizeof(*s));
+	write(fd, "\n", sizeof(char));
 }
