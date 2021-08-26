9d518f217e8b86dc173e26980bf106dc3ee1d3c0
Carmine Watcher <cwatcher@student.21-school.r>
2021-08-26 20:42:52 +0300

    [squash][bugfix] t(b).sh -= [  ]

--- tb.sh
+++ tb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # sample usage: ./tb.sh main.c cat "grep int" "wc -l"
 < $1 $2 | $3 | $4 > out.0.txt
-pipex "$1" "$2" "$3" "$4" out.txt
-[ 'diff out.0.txt out.txt' ] && echo "ok!"
+./pipex "$1" "$2" "$3" "$4" out.txt
+diff out.0.txt out.txt && echo "ok!"
