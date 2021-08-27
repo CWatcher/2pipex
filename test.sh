3199bcf0b7651c1903418235eb69334c1fbb9753
Carmine Watcher <CWatcher@student.21-school.r>
2021-08-27 16:07:51 +0300

    test.sh: separate error output

--- test.sh
+++ test.sh
@@ -1,26 +1,38 @@
 #!/bin/sh
 
+make pipex
+
+./t.sh main.c cat "wc -l"
+
+#$PATH
+
 [ ! -d bin1 ] && mkdir bin1
 [ ! -d bin2 ] && mkdir bin2
-ln -s /bin/ls bin2
 PATH_SAVED=$PATH
 PATH=bin1:bin2:$PATH
 
-./t.sh main.c ls wc 2> out.error.txt
+# cd bin1
+# ln -sf no_file ls
+ln -sf no_file bin1/ls
+# cd ..
+ln -sf /bin/ls bin2
+echo test1 error output: > out.error.txt
 
-echo "bad_format" > bin1/ls
+./t.sh main.c ls wc 2>> out.error.txt
+
+echo ---------------------------------- >> out.error.txt
+head -c 20 /bin/ls > bin1/ls
+tail -c +19 /bin/ls > bin1/ls
 chmod +x bin1/ls
-./t.sh main.c ls wc 2> out.error.txt
-cat out.error.txt
 
-cd bin1
-ln -sf no_file ls
-cd ..
-./t.sh main.c ls wc 2> out.error.txt
+echo test2 error output: >> out.error.txt
+
+./t.sh main.c ls wc 2>> out.error.txt
+
+echo ---------------------------------- >> out.error.txt
+
+echo "\n"
 cat out.error.txt
 
 PATH=$PATH_SAVED
-rm out.error.txt
 rm -rf bin1 bin2
-
-./t.sh main.c cat "wc -l"
