c36637d6cbfbccc2510bdcb08fb5e52482571615
Carmine Watcher <CWatcher@student.21-school.r>
2021-08-28 09:04:14 +0300

    [rf] test.sh: simplified

--- test.sh
+++ test.sh
@@ -1,38 +1,30 @@
 #!/bin/sh
 
-make pipex
+make pipex > /dev/null && echo make ok!
 
 ./t.sh main.c cat "wc -l"
 
 #$PATH
 
-[ ! -d bin1 ] && mkdir bin1
-[ ! -d bin2 ] && mkdir bin2
-PATH_SAVED=$PATH
-PATH=bin1:bin2:$PATH
+[ ! -d bin ] && mkdir bin
+PATH=bin:$PATH
+rm out.error.txt
 
-# cd bin1
-# ln -sf no_file ls
-ln -sf no_file bin1/ls
-# cd ..
-ln -sf /bin/ls bin2
+ln -sf no_file bin/ls
 echo test1 error output: > out.error.txt
 
 ./t.sh main.c ls wc 2>> out.error.txt
 
-echo ---------------------------------- >> out.error.txt
-head -c 20 /bin/ls > bin1/ls
-tail -c +19 /bin/ls > bin1/ls
-chmod +x bin1/ls
+echo -e "----------------------------------\n" >> out.error.txt
+head -c 20 /bin/ls > bin/ls
+tail -c +19 /bin/ls > bin/ls
+chmod +x bin/ls
 
 echo test2 error output: >> out.error.txt
 
 ./t.sh main.c ls wc 2>> out.error.txt
 
-echo ---------------------------------- >> out.error.txt
-
-echo "\n"
+echo -e "----------------------------------\n" >> out.error.txt
 cat out.error.txt
 
-PATH=$PATH_SAVED
-rm -rf bin1 bin2
+rm -rf bin
